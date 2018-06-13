ffc430b715b58877d09f911c29f42d638e2b66e8
john <johnpwoodiv@gmail.com>
2018-06-13 14:45:41 -0500

    reconfigure webpack and eslint

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,31 @@
 
 const path = require('path');
 
-module.exports = {
+module.exports = (env) => {
+  const production = env === 'production'
+  return ({
     entry: './src/app.js',
     output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-    }, {
-      test: /\.s?css$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-    }],
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  },
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+      }, {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      }],
+    },
+    devtool: production? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    },
+  })
 };
